ee61b21a6aa21173de1d56f674203bc4ce5ef22e
Ark kevin <101125125+arkkevin@users.noreply.github.com>
2024-06-08 12:33:05 +0700

    Add files via upload

---
+++ database/migrations/2022_12_15_131756_create_barangs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('namabarang');
+            $table->string('images');
+            $table->integer('harga');
+            $table->integer('stok');
+            $table->string('itemid')->default(rand(10,9));
+            $table->string('keterangan');
+            $table->integer('category_id');
+            $table->boolean('is_ready')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
